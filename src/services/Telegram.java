483ff4060561396a0a8d3c362a2932c170242efb
João Pedro Braz <joao.braz@ekaizen-br.com>
2020-06-30 23:23:27 -0300

    watson integration

--- src/services/Telegram.java
+++ src/services/Telegram.java
@@ -1,9 +1,14 @@
 package services;
 
 import intent.main.MainIntent;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,12 +16,34 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Voice;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileNotFoundException;
+import org.json.JSONObject;
+
 /**
  * Bot class. Forwards all the messages to the MainIntent.
  *
  * @author joao
  */
 public class Telegram extends TelegramLongPollingBot {
+
+    private final static String JSON_PATH = "./src/lib/telegram.json";
+    private String name, token;
+
+    public Telegram() {
+        try (FileReader reader = new FileReader(JSON_PATH)) {
+            JsonParser jsonParser = new JsonParser();
+            JsonObject obj = jsonParser.parse(reader).getAsJsonObject();
+
+            name = obj.get("name").getAsString();
+            token = obj.get("token").getAsString();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.toString());
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
 
     /**
      *
@@ -26,14 +53,36 @@ public class Telegram extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Message message = update.getMessage();
 
+        String messageText = message.getText();
+
+        /**
+         * If the given message is an Audio, we'll download it and send to be
+         * parsed by the Watson Speech to Text API
+         */
         if (message.getVoice() != null) {
             try {
                 Voice voice = message.getVoice();
-                InputStream audio = this.downloadFileAsStream(voice.getFileId());
+                URL url = new URL("https://api.telegram.org/bot" + getBotToken() + "/getFile?file_id=" + voice.getFileId() + "");
+                InputStream inputStream = url.openStream();
+                String filePath = "";
+                try {
+                    String fileData = IOUtils.toString(inputStream, "utf8");
+                    JSONObject obj = new JSONObject(fileData);
+                    filePath = obj.getJSONObject("result").get("file_path").toString();
+                } catch (IOException ex) {
+                    System.err.println(ex.toString());
+                } finally {
+                    inputStream.close();
+                }
+                InputStream audio = this.downloadFileAsStream(filePath);
                 Watson watson = new Watson();
-                watson.convert(audio);
+                messageText = watson.convert(audio);
             } catch (TelegramApiException ex) {
                 System.err.println(ex.toString());
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(Telegram.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Telegram.class.getName()).log(Level.SEVERE, null, ex);
             }
 
         }
@@ -45,7 +94,7 @@ public class Telegram extends TelegramLongPollingBot {
          */
         intent.IntentDTO response = (new MainIntent()).run(
                 message.getFrom().getId().toString(),
-                message.getFrom().getFirstName(), message.getText());
+                message.getFrom().getFirstName(), messageText);
         sendMessage(response.getMessage(), response.getIdTelegram());
     }
 
@@ -55,7 +104,7 @@ public class Telegram extends TelegramLongPollingBot {
      */
     @Override
     public String getBotUsername() {
-        return "FATEC_HELPER_BOT";
+        return name;
     }
 
     /**
@@ -64,7 +113,7 @@ public class Telegram extends TelegramLongPollingBot {
      */
     @Override
     public String getBotToken() {
-        return "1291405301:AAEGX5gQaD0aRa6TGu1Iw1hjzuXhxF-sccM";
+        return token;
     }
 
     /**
--- src/services/Watson.java
+++ src/services/Watson.java
@@ -5,21 +5,18 @@
  */
 package services;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionAlternative;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResult;
 import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -27,27 +24,28 @@ import org.xml.sax.SAXException;
  */
 public class Watson {
 
-    private static final String XML_PATH = "../lib/Watson.xml";
+    private static final String JSON_PATH = "./src/lib/watson.json";
     private String apiKey;
     private String serviceUrl;
     private final SpeechToText speechToText;
 
     public Watson() {
-        this.parseXML();
+        parseJSON();
         IamAuthenticator authenticator = new IamAuthenticator(apiKey);
         speechToText = new SpeechToText(authenticator);
         speechToText.setServiceUrl(serviceUrl);
     }
 
-    private void parseXML() {
-        try {
-            File inputFile = new File(XML_PATH);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            this.apiKey = doc.getDocumentElement().getAttribute("api-key");
-            this.serviceUrl = doc.getDocumentElement().getAttribute("service-url");
-        } catch (IOException | ParserConfigurationException | SAXException e) {
+    private void parseJSON() {
+        try (FileReader reader = new FileReader(JSON_PATH)) {
+            JsonParser jsonParser = new JsonParser();
+            JsonObject obj = jsonParser.parse(reader).getAsJsonObject();
+
+            apiKey = obj.get("api_key").getAsString();
+            serviceUrl = obj.get("service_url").getAsString();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.toString());
+        } catch (IOException e) {
             System.err.println(e.toString());
         }
     }
@@ -61,7 +59,15 @@ public class Watson {
 
         SpeechRecognitionResults speechRecognitionResults
                 = speechToText.recognize(recognizeOptions).execute().getResult();
-        System.out.println(speechRecognitionResults);
-        return "";
+        String voice = "";
+        for (SpeechRecognitionResult result : speechRecognitionResults.getResults()) {
+            if (result.isXFinal()) {
+                for (SpeechRecognitionAlternative alternative : result.getAlternatives()) {
+                    voice += alternative.getTranscript();
+                }
+            }
+        }
+        System.out.println(voice);
+        return voice;
     }
 }
